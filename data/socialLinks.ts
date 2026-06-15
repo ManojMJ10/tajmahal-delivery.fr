84210ae188e62450f7dd43e944b7ac226974e0e4
MANOJ KUMAR <manojkumar@mac-5.home>
2026-06-15 14:55:24 +0200

    Update socialmedia action links

--- data/socialLinks.ts
+++ data/socialLinks.ts
@@ -1,12 +1,12 @@
 export const socialLinks = {
-  menu: "/menu/takeaway",
+  menu: "/kiosk",
   mainSite: "/",
   call: "tel:0493730787",
   directions:
-    "https://www.google.com/maps/dir/?api=1&destination=The+Marina,+1001+Battery+Avenue,+06270+Villeneuve-Loubet",
-  instagram: "https://www.instagram.com/",
+    "https://www.google.com/maps/place/Restaurant+Taj+Mahal+Marina/@43.6356255,7.133814,361m/data=!3m1!1e3!4m22!1m15!4m14!1m6!1m2!1s0x12cc2b25c6f9365f:0x9634539c9030df74!2s1220+Av.+Saint-Philippe,+06410+Biot,+France!2m2!1d7.0802901!2d43.6158104!1m6!1m2!1s0x12cdd47d7abd3cc7:0x3a1a43ffdcf18cb2!2sMarina+Baie+des+Anges,+Bureau+du+Port+Espace+Maurice+Aubert,+06270+Villeneuve-Loubet,+France!2m2!1d7.1387956!2d43.635411!3m5!1s0x12cdd47d2036d729:0x87eaad47ae9228a4!8m2!3d43.6356255!4d7.1361958!16s%2Fg%2F11d_bbl5yv?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
+  instagram: "https://www.instagram.com/taj_mahal_marina/",
   facebook: "https://www.facebook.com/",
   tiktok: "https://www.tiktok.com/",
   googleReviews:
-    "https://www.google.com/maps/search/?api=1&query=Taj+Mahal+Marina+Villeneuve-Loubet",
+    "https://www.google.com/maps/place/Restaurant+Taj+Mahal+Marina/@43.6356255,7.1361958,722m/data=!3m1!1e3!4m18!1m9!3m8!1s0x12cdd47d2036d729:0x87eaad47ae9228a4!2sRestaurant+Taj+Mahal+Marina!8m2!3d43.6356255!4d7.1361958!9m1!1b1!16s%2Fg%2F11d_bbl5yv!3m7!1s0x12cdd47d2036d729:0x87eaad47ae9228a4!8m2!3d43.6356255!4d7.1361958!9m1!1b1!16s%2Fg%2F11d_bbl5yv?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
 } as const;
